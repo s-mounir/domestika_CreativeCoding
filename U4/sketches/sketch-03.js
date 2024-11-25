cb3062426765bc6e0d5e935418dd074c6fd0c8b1
Sara Mounir <saramounir.pro@gmail.com>
2024-11-25 16:50:43 +0100

    U4 : creation de la classe Agent

--- U4/sketches/sketch-03.js
+++ U4/sketches/sketch-03.js
@@ -5,12 +5,12 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
   const agents = [];
 
   for (let i = 0; i < 40; i++){
-    const x = random.range(O, width);
-    const y = random.range(O, height);
+    const x = random.range(0, width);
+    const y = random.range(0, height);
 
     agents.push(new Agent(x,y));
   }
@@ -19,11 +19,9 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const agentA = new Agent(800, 400);
-    const agentB = new Agent(300, 700);
-
-    agentA.draw(context);
-    agentB.draw(context);
+    agents.forEach(agent => {
+      agent.draw(context);
+    })
   };
 };
 
